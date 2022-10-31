fee5e40cfeef0f5f7cf9fd2d51d8eab6b6110de6
rm <rm8918351@gmail.com>
2022-10-31 21:42:50 +0530

    ,

--- scipy/__init__.py
+++ scipy/__init__.py
@@ -56,17 +56,17 @@ Utility tools
  __numpy_version__ --- Numpy version string
 
 """
-# import os
-
-# # start delvewheel patch
-# def _delvewheel_init_patch_1_1_0():
-#     import os
-#     libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'scipy.libs'))
-#     os.add_dll_directory(libs_dir)
 
 
-# _delvewheel_init_patch_1_1_0()
-# del _delvewheel_init_patch_1_1_0
+# start delvewheel patch
+def _delvewheel_init_patch_1_1_0():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'scipy.libs'))
+    os.add_dll_directory(libs_dir)
+
+
+_delvewheel_init_patch_1_1_0()
+del _delvewheel_init_patch_1_1_0
 # end delvewheel patch
 
 
